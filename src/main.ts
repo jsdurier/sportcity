6e6ad71b005d5332af2a127a15de56518d4e7c3f
jsdurier <jsdurier@gmail.com>
2023-01-09 00:10:18 +0100

    feat: add files for pwa compatibility

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { enableProdMode } from '@angular/core';
+import {
+	enableProdMode,
+	importProvidersFrom
+} from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import AppComponent from './app.component';
 import { environment } from './environments/environment';
@@ -12,6 +16,19 @@ bootstrapApplication(
 	AppComponent,
 	{
 		providers: [
+			importProvidersFrom(
+				ServiceWorkerModule.register(
+					'ngsw-worker.js',
+					{
+						enabled: environment.production,
+						/**
+						 * Register the ServiceWorker as soon as the app is stable
+						 * or after 30 seconds (whichever comes first).
+						 */
+						registrationStrategy: 'registerWhenStable:30000'
+					}
+				)
+			)
 		]
 	}
 );
